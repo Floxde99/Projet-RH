fbcd511c9445a38a521d5ccc4181de7602f50575
Floxde99 <florian.fchr@hotmail.com>
2025-06-20 13:54:17 +0200

    v0.9

--- services/authguard.js
+++ services/authguard.js
@@ -1,18 +1,29 @@
-const authguard = (req, res, next) => {
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient();
+
+const authguard = async (req, res, next) => {
     try {
-        const user = process.env.AG_VALUE
         if (req.session.user) {
-            return next();
-        }else throw { message: "Vous devez être connecté pour accéder à cette page" };
-    } catch (error) {
-        res.redirect("/login");
-        res.render("pages/login.twig", {
-            error: { message: error.message },
-            formData: {
-                email: req.body.email || "",
-                password: req.body.password || ""
+            // Correction: utiliser BusinessOwner au lieu de user
+            const userFound = await prisma.businessOwner.findUnique({
+                where: {
+                    id: req.session.user.id
+                }
+            });
+            
+            if (userFound) {
+                return next();
+            } else {
+                throw { message: "Utilisateur non trouvé" };
             }
-        });
+        } else {
+            throw { message: "Vous devez être connecté pour accéder à cette page" };
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect("/login");
     }
 }
+
 module.exports = authguard;
